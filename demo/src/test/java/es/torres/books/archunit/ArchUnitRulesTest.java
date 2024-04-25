03a0915bd42dcb4160d09a2a073c5c5da0dc9fc9
Andrés Torres <andres@kalyp.io>
2024-04-25 09:28:23 +0200

    fix: annotation processor for error-prone without no test will run fix: ignore tests in Demo using ArchIgnore (for demo purposes) fix: add plugin repositories so Zally and ArchUnit plugin can find the libraries in jitpack.io

--- demo/src/test/java/es/torres/books/archunit/ArchUnitRulesTest.java
+++ demo/src/test/java/es/torres/books/archunit/ArchUnitRulesTest.java
@@ -1,30 +1,39 @@
 package es.torres.books.archunit;
 
-//@Disabled
-//@AnalyzeClasses(packagesOf = BookApplication.class, importOptions = ImportOption.DoNotIncludeTests.class)
+import com.kalyp.buildingblocks.archunit.test.*;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchIgnore;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.junit.ArchTests;
+import es.torres.books.BookApplication;
+import org.junit.jupiter.api.Disabled;
+
+@ArchIgnore
+@AnalyzeClasses(packagesOf = BookApplication.class, importOptions = ImportOption.DoNotIncludeTests.class)
 public class ArchUnitRulesTest {
-//
-//    @ArchTest
-//    ArchTests architectureRules = ArchTests.in(AllArchitectureRules.class);
-//
-//    @ArchTest
-//    ArchTests configurationRules = ArchTests.in(AllConfigurationRules.class);
-//
-//    @ArchTest
-//    ArchTests controllerRules = ArchTests.in(AllControllerRules.class);
-//
-//    @ArchTest
-//    ArchTests dataMappingRules = ArchTests.in(AllDataMappingRules.class);
-//
-//    @ArchTest
-//    ArchTests generalCodingRules = ArchTests.in(AllGeneralCodingRules.class);
-//
-//    @ArchTest
-//    ArchTests loggingRules = ArchTests.in(AllLoggingRules.class);
-//
-//    @ArchTest
-//    ArchTests namingConventionRules = ArchTests.in(AllNamingConventionRules.class);
-//
-//    @ArchTest
-//    ArchTests relationalPersistenceRules = ArchTests.in(AllRelationalPersistenceRules.class);
+
+    @ArchTest
+    ArchTests architectureRules = ArchTests.in(AllArchitectureRules.class);
+
+    @ArchTest
+    ArchTests configurationRules = ArchTests.in(AllConfigurationRules.class);
+
+    @ArchTest
+    ArchTests controllerRules = ArchTests.in(AllControllerRules.class);
+
+    @ArchTest
+    ArchTests dataMappingRules = ArchTests.in(AllDataMappingRules.class);
+
+    @ArchTest
+    ArchTests generalCodingRules = ArchTests.in(AllGeneralCodingRules.class);
+
+    @ArchTest
+    ArchTests loggingRules = ArchTests.in(AllLoggingRules.class);
+
+    @ArchTest
+    ArchTests namingConventionRules = ArchTests.in(AllNamingConventionRules.class);
+
+    @ArchTest
+    ArchTests relationalPersistenceRules = ArchTests.in(AllRelationalPersistenceRules.class);
 }
